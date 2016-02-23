e47c31b7df49c3ffb3539a2e8e354e5c6850edca
Frederik Mainz <fm@inz.dk>
2016-02-23 19:28:00 +0100

    setup-controller
    Added controller

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('app',['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/' {
+    controller: 'HomeController',
+    templateUrl: 'views/home.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
