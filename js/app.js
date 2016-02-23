b1163514fc3ad6f56fa030ab7739308b1bf62aee
Frederik Mainz <fm@inz.dk>
2016-02-23 21:16:20 +0100

    knapper
    navbar

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('app',['ngRoute']);
 
 app.config(function($routeProvider){
   $routeProvider
-  .when('/' {
+  .when('/', {
     controller: 'HomeController',
     templateUrl: 'views/home.html'
   })
